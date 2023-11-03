facbf1cd73a0ca30b2daaae15faa026e0e249e2f
scn1901 <scnakamura2301@gmail.com>
2023-11-02 21:18:09 -1000

    oops

--- app/imports/ui/pages/ListContacts.jsx
+++ app/imports/ui/pages/ListContacts.jsx
@@ -8,8 +8,7 @@ import { Contacts } from '../../api/contact/Contacts';
 import Contact from '../components/Contact';
 import LoadingSpinner from '../components/LoadingSpinner';
 
-/* Renders a table containing all of the Stuff documents. Use <StuffItem> to render each row. */
-const ListStuff = () => {
+const ListContacts = () => {
   // useTracker connects Meteor data to React components. https://guide.meteor.com/react.html#using-withTracker
   const { ready, stuffs } = useTracker(() => {
     // Note that this subscription will get cleaned up
@@ -62,4 +61,4 @@ const ListStuff = () => {
   ) : <LoadingSpinner />);
 };
 
-export default ListStuff;
+export default ListContacts;
